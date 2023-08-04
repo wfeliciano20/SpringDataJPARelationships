a124b16f15a5f9d4d8af240ffcee457ded4203f0
wfeliciano20 <william.feliciano2@gmail.com>
2023-08-03 21:49:47 -0400

    Initial commit

---
+++ src/test/java/com/williamfeliciano/springdatajparelationships/SpringDataJpaRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamfeliciano.springdatajparelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
